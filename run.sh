a14b969c9dfd03bb6fb96833b6af57a0b525077a
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-09-20 13:58:35 +0200

    Python after conda

--- run.sh
+++ run.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 #SBATCH -M snowy
-module load python/3.11.4
+
+# Don't! Gives error:
+#
+# Lmod has detected the following error:  Cannot load module "conda/latest" because these module(s) are loaded:
+#    python
+#
+# module load python/3.11.4
+
 module load conda/latest
+module load python/3.11.4
 module load intel-oneapi
 module load init_opencl/2023.1.0"
 python opencl_test.py 
